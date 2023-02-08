a35283326a6690b1053fe6a86a22e56685bc74a7
sena_kuzuoka <senakuzuoka@gmail.com>
2023-02-08 13:03:20 +0900

    add sample namespace

--- Assets/Samples/Scritps/CameraController.cs
+++ Assets/Samples/Scritps/CameraController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-    [SerializeField] float rotateSpeed = 1.0f;
-
-    void Update()
+namespace tarakoKutibiru.UnityBillboard.Samples
+{
+    public class CameraController : MonoBehaviour
     {
-        var y = Input.GetAxis("Horizontal") * rotateSpeed;
-        transform.RotateAround(Vector3.zero, Vector3.up, y);
+        [SerializeField] float rotateSpeed = 1.0f;
 
-        var x = Input.GetAxis("Vertical") * rotateSpeed;
-        transform.RotateAround(Vector3.zero, this.transform.right, x);
+        void Update()
+        {
+            var y = Input.GetAxis("Horizontal") * rotateSpeed;
+            transform.RotateAround(Vector3.zero, Vector3.up, y);
+
+            var x = Input.GetAxis("Vertical") * rotateSpeed;
+            transform.RotateAround(Vector3.zero, this.transform.right, x);
+        }
     }
 }
